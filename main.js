ffc693ca75a0395ebf53f8854b8e8f2c89d05dfd
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-09 14:14:31 +0100

    inicializálás kész

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 document.getElementById("DOMContentLoaded", init);
 
-let mintaszovegId, betumeretId, szovegszinId, hatterszinId;
+let mintaszovegId, betumeretId, szovegszinId, hatterszinId, alaphelyzetGomb;
+
+function init() {
+    document.getElementById("mintaszovegId").addEventListener("change", mintaszoveg);
+    document.getElementById("betumeretId").addEventListener("change", betumeret);
+    document.getElementById("szovegszinId").addEventListener("change", szovegszin);
+    document.getElementById("hatterszinId").addEventListener("change", hatterszin);
+    document.getElementById("alaphelyzetGomb").addEventListener("click", alaphelyzet);
+}
+
+function mintaszoveg() {
+
+}
+
+function betumeret() {
+
+}
+
+function szovegszin() {
+
+}
+
+function hatterszin() {
+
+}
+
+function alaphelyzet() {
+
+}
